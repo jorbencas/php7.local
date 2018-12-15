7a28f4fe3f6478536b22a4635ad413cddc168985
jorbencas <jorbencas@gmail.com>
2018-12-15 14:27:58 +0100

    Deleting innecesary files

--- controllers/about.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require  __DIR__.'/../utils/utils.php';
-    require  __DIR__.'/../views/about.view.php';
--- controllers/single_post.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require __DIR__.'/../utils/utils.php';
-    require __DIR__.'/../views/single_post.view.php';
